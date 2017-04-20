ccd026631c11bc6607e521d4383480dc20f11e9b
Janos Gabler <janos.gabler@gmail.com>
2017-04-20 18:31:23 +0200

    First running version of anki_utilities as conda package.

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
+pkg = find_packages()
+print('\n\n\nthis is the package list:\n', pkg, '\n\n')
+
 setup(
     name='anki_utilities',
-    version='0.0.001',
-    packages=find_packages()
+    version='0.0.007',
+    packages=pkg
 )
